312b65da37d6786163593ea9af6d6fe67744926e
name <email...>
2026-01-23 19:55:47 +0500

    Router

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  runtimeConfig: {
+    token: "",
+    public: {
+      apiurl: ""
+    },
+    
+  },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxt/scripts'
+  ]
+})
