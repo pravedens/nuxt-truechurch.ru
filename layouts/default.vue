6ebfa6532008d4a426de551a74a2ab558ef8cc2e
name <email...>
2026-01-29 19:50:54 +0500

    Header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import LayoutFooter from '~/components/LayoutFooter.vue';
+import LayoutHeader from '~/components/LayoutHeader.vue';
 
 </script>
 
 <template>
     <div>
-        <div>Шапка</div>
+        <layout-header />
         <slot />
         <layout-footer />
     </div>
