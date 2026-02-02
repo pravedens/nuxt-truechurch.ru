0010385a05c84e01981c5ed8905666591f580470
name <email...>
2026-02-02 19:20:36 +0500

    Refresh данных

--- pages/catalog/index.vue
+++ pages/catalog/index.vue
@@ -1,23 +1,33 @@
 <script setup lang="ts">
 import type { GetCategoriesResponse } from '~/interfaces/category.interface';
+
 const config = useRuntimeConfig();
 const API_URL = config.public.apiurl;
+const input = ref("");
 
-const { data, error } = await useFetch<GetCategoriesResponse>(API_URL + "/categories");
+const { data, error, refresh } = await useFetch<GetCategoriesResponse>(
+    API_URL + "/categories",
+    {
+        method: "POST",
+        body: input,
+        immediate: false,
+        watch: false,
+    }
+);
 
     console.log(data.value);
     console.log(error.value);
-//try {
-//    const data = await $fetch<GetCategoriesResponse>(API_URL + "/categories");
-//    console.log(data);
-//} catch (e) {
-//    console.error(e);
-//}
+
+    async function sendData() {
+        refresh();
+    }
 
 </script>
 
 <template>
     <div>
         Catalog
+        <InputField v-model="input" variant="black" />
+        <button @click="sendData">Отправить</button>
     </div>
 </template>
