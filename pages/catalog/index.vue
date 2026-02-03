b8366dc6bb1b7f101afa36fd8e6fdd88fe2d6e60
name <email...>
2026-02-03 19:45:56 +0500

    Вывод категорий в каталоге

--- pages/catalog/index.vue
+++ pages/catalog/index.vue
@@ -1,44 +1,47 @@
 <script setup lang="ts">
-import type { GetCategoriesResponse } from '~/interfaces/category.interface';
+import type { GetCategoriesResponse } from "~/interfaces/category.interface";
 
 const config = useRuntimeConfig();
 const API_URL = config.public.apiurl;
-const input = ref("");
 const select = ref("");
 
-await useFetch<GetCategoriesResponse>(
-    API_URL + "/categories",
-    {
-        method: "POST",
-        body: input,
-        immediate: false,
-        watch: false,
-    }
-);
+const { data } = await useFetch<GetCategoriesResponse>(API_URL + "/categories");
 
+const selectDefault = { value: '', label: 'Категории' }
 
+const categoriesSelect = computed(() => {
+    return data.value
+        ? data.value?.categories.map((c) => ({
+        value: c.id.toString(),
+        label: c.name,
+    }))
+    .concat(selectDefault)
+    : [selectDefault];
+})
 </script>
 
 <template>
-    <div>
-        Catalog
-        <SelectField 
-        v-model="select"
-            :options="[
-                {
-                    value: '',
-                    label: 'Категории',
-                },
-                {
-                    value: '1',
-                    label: 'Первый',
-                },
-                                {
-                    value: '2',
-                    label: 'Второй',
-                },
-            ]"
-            
+  <div>
+    <h1 class="left">Каталог товаров</h1>
+    <div class="catalog">
+      <div class="catalog__filter">
+        <SelectField
+          v-model="select"
+          :options="categoriesSelect"
         />
+      </div>
+      <div></div>
     </div>
+  </div>
 </template>
+
+<style scoped>
+.catalog {
+    display: flex;
+    gap: 36px;
+}
+
+.catalog__filter {
+    width: 260px;
+}
+</style>
