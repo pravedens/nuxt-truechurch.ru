7e77a5ec8b382aae28e152c984d91cf3dda627ff
name <email...>
2026-02-07 14:57:42 +0500

    Store для избранного

--- pages/favorites.vue
+++ pages/favorites.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import { useFavoritesStore } from '~/stores/favorites.store';
+const favoriteStore = useFavoritesStore();
+favoriteStore.addToFavorite(1);
+</script>
+
 <template>
     <div>
-        Favorites
+        {{ favoriteStore.favoriteIds[0] }}
     </div>
 </template>
