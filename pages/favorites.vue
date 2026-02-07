a238065d4089778abfd860f2ef6aaac2ba00df84
name <email...>
2026-02-07 17:23:15 +0500

    Отображение избранного

--- pages/favorites.vue
+++ pages/favorites.vue
@@ -1,7 +1,36 @@
+<script setup lang="ts">
+import type { Product } from "~/interfaces/product.interface";
 
+const favoriteStore = useFavoritesStore();
+const config = useRuntimeConfig();
+const API_URL = config.public.apiurl;
+const products = ref<Product[]>();
+
+watchEffect(async () => {
+  const data = await Promise.all(
+    favoriteStore.favoriteIds.map((id) => {
+      return $fetch<{ product: Product }>(API_URL + "/products/" + id);
+    }),
+  );
+  products.value = data.map((el) => el.product);
+});
+</script>
 
 <template>
-    <div>
-        Fav
-    </div>
+  <div class="catalog__grid">
+    <CatalogCard
+      v-for="product in products"
+      :key="product.id"
+      v-bind="product"
+    />
+  </div>
 </template>
+
+<style scoped>
+.catalog__grid {
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 64px 12px;
+}
+</style>
